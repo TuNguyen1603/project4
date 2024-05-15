f8c7fcad82461a4eeead0c63e7d0da3181d7b8c6
Kuroko1603 <vantuktiger1998@gmail.com>
2024-05-15 15:52:01 +0700

    cmt-tunv35

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'wj3c7p6rq6'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-4u0hwa8yfqcmmcrv.us.auth0.com',
+  clientId: '1jAshUay0L0sxAqqAm8JP3reFxXUuWvL',
+  callbackUrl: 'http://localhost:3000/callback'
+}
